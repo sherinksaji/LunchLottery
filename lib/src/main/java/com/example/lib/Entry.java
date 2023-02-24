ab644a30a0ce01ca77cc105b554dc7bef06dfdad
sherinksaji <sherinkaruvallilsaji@gmail.com>
2023-02-24 22:21:48 +0800

    second commit: added Entry Class

--- lib/src/main/java/com/example/lib/Entry.java
+++ lib/src/main/java/com/example/lib/Entry.java
@@ -1,4 +1,14 @@
 package com.example.lib;
 
 public class Entry {
+    /**
+     * Entry takes in inputs Telehandle, lunchStartTime,
+     * and Date
+     * This object is used to collect user input
+     * This object will be written into firebase database
+     * This object will be read back into the app
+     * This object will be sorted according to a comparator
+     * Sort according to what? Telehandle?
+     *
+     */
 }
