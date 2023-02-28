2897345b8115430b5216474f11cea4fde31b1a9a
sherinksaji <sherinkaruvallilsaji@gmail.com>
2023-02-28 15:04:51 +0800

    Entry class with Gregorian Calendar

--- lib/src/main/java/com/example/lib/Entry.java
+++ lib/src/main/java/com/example/lib/Entry.java
@@ -1,5 +1,9 @@
 package com.example.lib;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
 public class Entry {
     /**
      * Entry takes in inputs Telehandle, lunchStartTime,
@@ -11,4 +15,46 @@ public class Entry {
      * Sort according to what? Telehandle?
      *
      */
+
+    String telegramHandle;
+    long bookingTimeStamp;// the time that the user booked for
+
+    public Entry(String telegramHandle, GregorianCalendar gregCal) {
+        this.telegramHandle = telegramHandle;
+        this.bookingTimeStamp = gregCal.getTime().getTime();
+    }
+
+    public String getTelegramHandle() {
+        return telegramHandle;
+    }
+
+    public void setTelegramHandle(String telegramHandle) {
+        this.telegramHandle = telegramHandle;
+    }
+
+    private long getBookingTimeStamp() {
+        return this.bookingTimeStamp;
+    }
+
+    private void setBookingTimeStamp(GregorianCalendar gregCal) {
+        this.bookingTimeStamp = gregCal.getTime().getTime();
+    }
+
+    private GregorianCalendar getGregCal(){
+        GregorianCalendar gregCal = new GregorianCalendar();
+        gregCal.setTimeInMillis(this.bookingTimeStamp);
+        return gregCal;
+    }
+
+    public String calStr (){
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return dateFormat.format(this.getGregCal());
+    }
+
+    @Override
+    public String toString() {
+        return "ENTRY: \n" +
+                "TelegramHandle:" + this.getTelegramHandle() + '\n' +
+                "Timing:" + this.calStr() + '\n';
+    }
 }
