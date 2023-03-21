a5bd101c3e9835b42c5edbd345f32141d2cc46a8
jalamie <jamie.yunni@gmail.com>
2023-03-22 03:07:09 +0800

    updated pair

--- lib/src/main/java/com/example/lib/Pair.java
+++ lib/src/main/java/com/example/lib/Pair.java
@@ -3,30 +3,24 @@ package com.example.lib;
 import java.util.ArrayList;
 
 public class Pair {
-    private ArrayList <Entry> pair=new ArrayList<>();
-    public Pair(Entry entry1,Entry entry2) {
-        this.pair = pair;
-        pair.add(entry1);
-        pair.add(entry2);
+    private String id;
+    private String partner;
+
+    public Pair(String id, String partner) {
+        this.id = id; //yourself
+        this.partner = partner; //person paired with
     }
 
-    public boolean inPair(String telegramHandle){
-        for (Entry e:this.pair){
-            if (telegramHandle.equals(e.getTelegramHandle())){
-                return true;
-            }
-        }
-        return false;
+    public Pair(String id, String partner, String partner2) { //only used when entry array list is odd numbered
+        this.id = id;
+        this.partner = partner + ", " + partner2;
     }
 
-    public Entry match(String telegramHandle){
-        //only to be used if inPair is true
-        if (this.pair.get(0).getTelegramHandle()==telegramHandle){
-            return this.pair.get(1);
-        }
-        else return this.pair.get(0);
+    public String getId() {
+        return id;
     }
 
-
-
+    public String getPartner() {
+        return partner;
+    }
 }
