12fc500921232f0ca4e05f331fd9442140dce075
jalamie <jamie.yunni@gmail.com>
2023-04-04 00:00:11 +0800

    updated the populated slot page
    sign in button does not work yet, display does not show selected time

--- app/src/main/java/com/example/androidapp/ViewPopulatedSlots.java
+++ app/src/main/java/com/example/androidapp/ViewPopulatedSlots.java
@@ -2,6 +2,8 @@ package com.example.androidapp;
 
 import static android.content.ContentValues.TAG;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,11 +16,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lib.Countable;
 import com.example.lib.CreateSlots;
-import com.example.lib.LotteryEntry;
 import com.example.lib.LotteryTicket;
-import com.example.lib.PopulatedSlot;
 import com.example.lib.Slottable;
-import com.example.lib.Ticket;
 import com.example.lib.Week;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,34 +31,49 @@ public class ViewPopulatedSlots extends AppCompatActivity{
 
 
    DatabaseReference ref;
-    TextView nobodyJoinedTV;
-
+    TextView nobodyJoinedTV, button;
+    pl.droidsonroids.gif.GifImageView photo;
     ArrayList<Slottable> allTicketsUnderWeekX;
     ArrayList<Countable> populatedSlots;
     RecyclerView.Adapter<PopulatedSlotAdapter.SlotsHolder> populatedSlotAdapter;
-    String weekNode;
-
+    String weekNode,priorInput;
     RecyclerView recyclerView;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_populated_slots);
         allTicketsUnderWeekX =new ArrayList<>();
         populatedSlots=new ArrayList<>();
-
+        photo = findViewById(R.id.Photo);
+        Intent intent=getIntent();
+        priorInput=intent.getStringExtra("priorInput");
 
         /**
          *needed Week Method: public String weekForViewResult ()
          */
         weekNode=new Week.NextWeek().getWeekTitle();
         ref = FirebaseDatabase.getInstance().getReference().child(weekNode);
-        nobodyJoinedTV=(TextView) findViewById(R.id.nobodyJoinedTV);
-        readWeek();
+        //nobodyJoinedTV=(TextView) findViewById(R.id.nobodyJoinedTV);
+        nobodyJoinedTV = findViewById(R.id.nobodyJoinedTV);
+
+        //photo = findViewById(R.id.Photo);
+        button = findViewById(R.id.signUp);
 
         recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
         populatedSlotAdapter=new PopulatedSlotAdapter(this,populatedSlots);
         recyclerView.setLayoutManager( new LinearLayoutManager(this));
         recyclerView.setAdapter(populatedSlotAdapter);
+        readWeek();
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewPopulatedSlots.this,InputActivity.class));
+                finish();
+            }
+        });
     }
 
     private void readWeek(){
@@ -72,7 +86,10 @@ public class ViewPopulatedSlots extends AppCompatActivity{
                 allTicketsUnderWeekX.clear();
                 populatedSlots.clear();
                 if (dataSnapshot.exists()){
-                    nobodyJoinedTV.setVisibility(View.GONE);
+                    //nobodyJoinedTV.setVisibility(View.GONE);
+                    nobodyJoinedTV.setText("Your selected time is" +priorInput);
+                    photo.setVisibility(View.GONE);
+                    button.setVisibility(View.GONE);
                     String readWeekStr="";
                     for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
                         LotteryTicket ticket = postSnapshot.getValue(LotteryTicket.class);
@@ -88,8 +105,9 @@ public class ViewPopulatedSlots extends AppCompatActivity{
                     Log.i("Adapter", "notifyDataSetChanged() called");
                 }
                 else{
-
                     nobodyJoinedTV.setVisibility(View.VISIBLE);
+                    photo.setVisibility(View.VISIBLE);
+                    button.setVisibility(View.VISIBLE);
 
                 }
             }
